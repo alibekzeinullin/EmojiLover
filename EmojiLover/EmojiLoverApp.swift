085867d216781f44290efdc75313b4abd1d805ab
alibekzeinullin <alibekzeinullin@gmail.com>
2024-09-04 16:23:41 +0900

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Admin on 03.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
